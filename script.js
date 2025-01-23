c6b60e0a6fe63e156ba4251a010505110da3123c
alessiozaccato <alessiozaccato@gmail.com>
2025-01-23 15:12:49 +0100

    SOLVE: basic exercise

--- script.js
+++ script.js
@@ -36,3 +36,36 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+let card = document.getElementById("dinamicCard")
+let intestationMessage = document.getElementById("intestation");
+intestationMessage.innerHTML = "BoolTECH è una società specializzata in Coding. Offriamo soluzioni informatiche innovative e personalizzate per Html e Css. Grazie alla nostra esperienza e alla nostra passione per la tecnologia, aiutiamo i nostri clienti a raggiungere i loro obiettivi. Contattaci per scoprire come possiamo aiutarti. "
+intestationMessage.classList.add("text-warning")
+
+function addCard() {
+  for (i = 0; i < teamMembers.length; i++) {
+    let element = teamMembers[i];
+    card.innerHTML += `
+  <section id="dinamicCard" class="col-4">
+
+    <div class="row align-items-center">
+      <div class="col-6">
+        <figure>
+          <img width="100%" src="${element.img}" alt="${element.name}">
+        </figure>
+      </div>
+      <div class="col-6">
+        <p class="fs-2 text-white">${element.name}</p>
+        <p class="fs-4">${element.role}</p>
+        <a class="text-white" href="#">${element.email}</a>
+      </div>
+    </div>
+
+  </section>
+  `
+  }
+}
+
+addCard();
+
+
